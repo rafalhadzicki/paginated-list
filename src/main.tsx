3ef8f2b8992ded8deb6994c1f56537b7085b4b9b
Rafal Hadzicki <pzx113060@student.wsb.poznan.pl>
2024-03-08 00:34:22 +0100

    add providers, configure theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from '@/App.tsx';
+import '@/i18n/i18n.ts';
+import '@/index.css';
+import appTheme from '@/styles/theme.ts';
+import { ThemeProvider } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={appTheme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+const appTheme = createTheme({
+  palette: {
+    grey: {
+      100: '#F9F9F9',
+      200: '#F5F5F5',
+      300: '#F5F5F5',
+    },
+  },
+});
+
+export default appTheme;
